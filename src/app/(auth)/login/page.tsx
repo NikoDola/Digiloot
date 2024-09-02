5c6615a6411a55a42a6cbee36ced627c0cfc8ea7
nikola <nikodola@gmail.com>
2024-09-02 17:55:56 +0200

    login - css

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -46,33 +46,37 @@ export default function Login() {
     }
 
     return (
-        <main>
-            <Image className="logo" src='/branding/logo/snipsnap_vertical-logo.svg' width={200} height={200} alt="Logo" />
+        <main >
+            <p className="text-4xl m-0 text-center mt-16">Login</p>
+            <p className=" text-center mt-3" >Dont have an account? <Link className="text-blue-500" href='/login/signin'> Create one!</Link> </p>          
+            <div className="formWrapper">
+                <form  className="logInSignInForm">
+                    <div onClick={googleSign} className="buttonSignUp">
+                        <Image src={"/branding/icons/google.svg"} width={16} height={16} alt="google icon"/>
+                        <p>Login with Google</p>
+                    </div>
+                    <div onClick={gitHubSign} className="buttonSignUp">
+                        <Image src={"/branding/icons/github.svg"} width={16} height={16} alt="google icon"/>
+                        <p>Login with Github</p>
+                    </div>
+                </form>
 
-            <form onSubmit={Log} className="logInSignInForm">
-            <div onClick={googleSign} className="buttonSignUp">
-                <Image src={"/branding/icons/google.svg"} width={16} height={16} alt="google icon"/>
-                <p>Login with Google</p>
+                <p className="bettweenForm my-6">or</p>
+
+                {/* Email login */}
+                <form onSubmit={Log} className="logInSignInForm ">
+                    <input className="input" type="text" name="email" placeholder="email" required />
+                    <div className="password">
+                    <div onClick={handleShowPassword} >
+                        <Image className="icon cursor-pointer" src={showPassword ? '/branding/icons/visual.svg' : '/branding/icons/blind.svg'} height={16} width={16} alt="visual"/>
+                    </div>
+                    <input className="w-full" type={showPassword ? 'text': 'password'} name="password" placeholder="password" required />
+                    </div>
+                    <button className="mainButton" type="submit">Login</button>
+                    <p>Lost your password? <Link className="text-blue-500" href='/login/reset-password'> Reset your password!</Link></p>
+                </form>     
+                {error && <p style={{ color: 'red' }}>{error}</p>}
             </div>
-            <div onClick={gitHubSign} className="buttonSignUp">
-                <Image src={"/branding/icons/github.svg"} width={16} height={16} alt="google icon"/>
-                <p>Login with Github</p>
-            </div>
-                <p className="bettweenForm">or login with email and password</p>
-                <input className="input" type="text" name="email" placeholder="email" required />
-                <div className="password">
-                <div onClick={handleShowPassword}  className="w-14 h-full ">
-                <Image className="icon cursor-pointer" src={showPassword ? '/branding/icons/visual.svg' : '/branding/icons/blind.svg'} height={16} width={16} alt="visual"/>
-                </div>
-                <input className="w-full" type={showPassword ? 'text': 'password'} name="password" placeholder="password" required />
-                </div>
-                <button className="mainButton" type="submit">Login</button>
-            </form>     
-            <p className="my-4">Lost your password? <Link className="text-blue-500" href='/login/reset-password'> Reset your password!</Link> </p>
-            <p className="my-4"> Dont have an account? <Link className="text-blue-500" href='/login/signin'> Create one!</Link> </p>          
-            <form className="logInSignInForm">
-            </form>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
         </main>
     );
 }
