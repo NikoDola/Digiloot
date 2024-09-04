e42f70e23058aec487b2aa13dcf5b3b508c8e009
nikola <nikodola@gmail.com>
2024-09-04 22:55:25 +0200

    path not fixed

--- src/components/Bouncer.tsx
+++ src/components/Bouncer.tsx
@@ -5,17 +5,18 @@ import { useEffect } from "react";
 import { usePathname } from 'next/navigation'
 
 export default function Bouncer() {
-  const { user } = useUser();
-  const pathname = usePathname()
-  const router = useRouter();
+const { user } = useUser();
+    const pathname = usePathname()
+    const router = useRouter();
+
 
   useEffect(() => {
     if (!user) {
       router.push('/login');
+    }else{
+        router.push(pathname)
     }
-    return(
-        router.push(`${pathname}`)
-    )
+
   }, [user, router]);
 
   return (
